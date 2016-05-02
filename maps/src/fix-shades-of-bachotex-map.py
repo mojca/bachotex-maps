2cc20a4a1c593a7d85c36e076b22bc18a8578316
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2016-05-02 02:03:42 +0200

    fix the png graphic

--- maps/src/fix-shades-of-bachotex-map.py
+++ maps/src/fix-shades-of-bachotex-map.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
 
 import numpy as np
-import matplotlib
+# import matplotlib
 import matplotlib.image
-# import matplotlib.image as mpimg
-# from PIL import Image
+from PIL import Image
 
 filename_in  = "../png/bachotex-bw.png"
 filename_out = "../png/bachotex-bw-corrected-layers.png"
 
 im = matplotlib.image.imread(filename_in)
-# img = Image.open(filname_in)
+# im = Image.open(filename_in)
 
-it = np.round(im[:,:,0]*10)
-# it2 = it - 6*(it==10) + 1 + 64
-# it3 = np.array(it2).transpose()
+it = np.round(im[:,:,0]*10).astype(np.uint8)
 
-# lake: 62
-# sand: 1->64
-# paths: 3->64, 4->64
-# grass: 2->65
-# fence: 5->67
-# house: 10->66
-it2 = it + 62*(it==0) + 63*(it==1) + 61*(it==3) + 62*(it==4) + 63*(it==2) + 62*(it==5) + 56*(it==10)
-it3 = np.array(it2).transpose()
+it0 = np.zeros(np.shape(it))
 
-matplotlib.image.imsave(filename_out, it3, vmin=0,vmax=128, cmap='gray')
+num_lake =  0
+num_sand =  1
+num_gras =  2
+num_path =  3
+num_Path =  4
+num_fenc =  5
+num_hous = 10
 
-# it = it.astype(np.uint8)
-# matplotlib.image.imsave(filename_out, it, cmap='gray')
+col_lake =  -10
+col_sand =   1
+col_gras =   4
+col_path =   5 # grass
+col_Path =   2 # Path
+col_fenc =   7
+col_hous =   6 # house
 
+it2 = it0 + \
+    1*col_lake*(it==num_lake) + \
+    1*col_sand*(it==num_sand) + \
+    1*col_gras*(it==num_gras) + \
+    1*col_path*(it==num_path) + \
+    1*col_Path*(it==num_Path) + \
+    1*col_fenc*(it==num_fenc) + \
+    1*col_hous*(it==num_hous) + \
+    128
+
+j = Image.fromarray(it2.transpose().astype(np.uint8))
+j.save(filename_out)
+
+# for i in range(255):
+#     t = np.sum(it2==i)
+#     if t > 0:
+#         print(i,t)
