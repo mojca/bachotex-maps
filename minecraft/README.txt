e83b6d1cd997aaa9531ce62f20d95dd46e0ba04a
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2016-05-02 02:06:04 +0200

    add worlds

---
+++ minecraft/README.txt
@@ -0,0 +1,12 @@
+python2.7 ../soft/pymclevel/mce.py worlds/bachotex
+
+> heightmap ../maps/png/bachotex-bw-corrected-layers.png
+
+ INFO:You probably want to change your spawn point. I suggest (150, 128, 211)
+
+> spawn (150, 128, 211)
+> save
+> quit
+
+overviewer.py-2.7 worlds/bachotex google-maps/bachotex
+
---
+++ minecraft/google-maps/bachotex/overviewerConfig.js
@@ -0,0 +1,65 @@
+var overviewerConfig = {
+    "worlds": [
+        "world"
+    ], 
+    "tilesets": [
+        {
+            "spawn": [
+                0, 
+                128, 
+                200
+            ], 
+            "isOverlay": false, 
+            "last_rendertime": 1462146706, 
+            "name": "Overviewer Render (lighting)", 
+            "poititle": "Markers", 
+            "north_direction": 0, 
+            "minZoom": 0, 
+            "bgcolor": "#1a1a1a", 
+            "zoomLevels": 6, 
+            "base": "", 
+            "imgextension": "png", 
+            "defaultZoom": 1, 
+            "world": "world", 
+            "maxZoom": 6, 
+            "path": "world-lighting", 
+            "showlocationmarker": true
+        }
+    ], 
+    "CONST": {
+        "mapDivId": "mcmap", 
+        "UPPERLEFT": 0, 
+        "tileSize": 384, 
+        "UPPERRIGHT": 1, 
+        "image": {
+            "defaultMarker": "signpost.png", 
+            "queryMarker": "https://google-maps-icons.googlecode.com/files/regroup.png", 
+            "signMarker": "signpost_icon.png", 
+            "spawnMarker": "https://google-maps-icons.googlecode.com/files/home.png", 
+            "bedMarker": "bed.png"
+        }, 
+        "LOWERRIGHT": 2, 
+        "LOWERLEFT": 3, 
+        "regionStrokeWeight": 2
+    }, 
+    "map": {
+        "debug": true, 
+        "north_direction": "lower-left", 
+        "controls": {
+            "spawn": true, 
+            "mapType": true, 
+            "compass": true, 
+            "coordsBox": true, 
+            "overlays": true, 
+            "searchBox": true, 
+            "zoom": true, 
+            "pan": true
+        }, 
+        "center": [
+            -314, 
+            67, 
+            94
+        ], 
+        "cacheTag": "1462146890"
+    }
+};
